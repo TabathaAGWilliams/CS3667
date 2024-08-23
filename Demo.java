2a44354a5bb045d2f549e9edc1ab8f32254baedd
TabathaAGWilliams <geltta@appstate.edu>
2024-08-23 09:08:42 -0400

    worked on constructors

--- Demo.java
+++ Demo.java
@@ -4,6 +4,9 @@ public class Demo {
     public static void main(String[] args)
     {
         Scanner kb = new Scanner(System.in);
-        
+        System.out.println("Hello! What kind of pet do you have?");
+        String pet = kb.nextLine();
+        System.out.println("Wonderful! What is your pet's name?");
+        String petName = kb.nextLine();
     }
 }
--- Pet.java
+++ Pet.java
@@ -1,7 +1,21 @@
 public abstract class Pet {
     protected String name;
 
+    /**
+     * One-arg constructor.
+     * @param name type of pet
+     */
     public Pet(String name) {
         this.name = name;
     }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public abstract void makeNoise();
 }
